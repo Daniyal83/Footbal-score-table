c2cf938644a0d53a372569d4226adf3bbff44ba1
Daniyal83 <da-lu@lenta.ru>
2017-09-03 14:51:18 +0300

    Update football.js
    Added: a logic that does not allow to choose the same Team in both <select>s; updated points add to the selected Team, but still remains the case "Draw"; finally got the part that responsible for sorting Teams in list from Max at the top to Min at the bottom( thanks stackoverflow for help).

--- football.js
+++ football.js
@@ -3,30 +3,37 @@ var inputs = $("li input");
 
 function gameScore() {
 	    var button = document.querySelector("button");
-	    var team1Score = 0;
-	    var team2Score = 0;
+	    
 	    button.addEventListener("click", function() {
         var firstScore = parseInt(document.getElementById("first").value);
         var secondScore = parseInt(document.getElementById("second").value);
-            
+        
+        if($("#sel1 option:selected").text() === $("#sel2 option:selected").text()) {
+            alert("You should choose different Teams!!");
+        } else {
+
         if(firstScore > secondScore) {
         	alert($("#sel1 option:selected").text()+" won!");
             
-            for(var i = 0; i < 4; i++) {
+            for(var i = 0; i < inputs.length; i++) {
         if($("#sel1 option:selected").text() === teams[i]) {
-        inputs[i].value = team1Score + 3;
-        team1Score = parseInt(inputs[i].value);
+        inputs[i].value = parseInt(parseInt(inputs[i].value) + 3);
+        
              }
         }
 
-        	//document.getElementById("team1").value = team1Score + 3;
-        	//team1Score = parseInt(document.getElementById("team1").value);
+        	
                             
         } else if(firstScore < secondScore) {
         	alert($("#sel2 option:selected").text()+" won!");
-        	document.getElementById("team2").value = team2Score + 3;
-        	team2Score = parseInt(document.getElementById("team2").value);
-                
+
+            for(var i = 0; i < inputs.length; i++) {
+        if($("#sel2 option:selected").text() === teams[i]) {
+        inputs[i].value = parseInt(parseInt(inputs[i].value) + 3);
+        
+             }
+        }
+        	                
             
         } else {
         	alert("Draw!");
@@ -35,24 +42,12 @@ function gameScore() {
         	team1Score = parseInt(document.getElementById("team1").value);
         	team2Score = parseInt(document.getElementById("team2").value);
         }
-         if(team1Score>team2Score) {
-          $("#list li#two").before($("#list li#one"));
-        }
-    else if(team2Score>team1Score) {
-          $("#list li#one").before($("#list li#two"));
-        }
-    });   
+          }
+        let a = [], b;
+        [].forEach.call(document.querySelectorAll("ol > li"), (c)=>a.push([c.children[0].value,c]));
+        b = a.sort((a,b)=>b[0]-a[0]);
+        b.forEach((c)=>c[1].parentNode.appendChild(c[1]));
+   });   
 
 }
 gameScore();
-
-//for(var i = 0; i < 4; i++) {
-//    if($("#sel1 option:selected").text() == teams[i]) {
-//        inputs[i].value = team1Score + 3;
-//        team1Score = inputs[i].value;
-//    }
-//}
-
-
-
-
